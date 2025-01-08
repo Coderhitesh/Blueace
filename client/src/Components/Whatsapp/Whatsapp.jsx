97fda06e10b3c7805449a5a93fc3eb457fbb452e
Coderhitesh <hiteshy468@gmail.com>
2025-01-08 13:19:15 +0530

    change whatsapp number

--- client/src/Components/Whatsapp/Whatsapp.jsx
+++ client/src/Components/Whatsapp/Whatsapp.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import './whatsapp.css'
 
 function Whatsapp() {
-  const phoneNumber = "9079036042";
+    const phoneNumber = "9311539090";
 
-  return (
-    <a
-      href={`https://api.whatsapp.com/send?phone=${phoneNumber}`}
-      className="whatsapp-button"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Chat on WhatsApp"
-    >
-      <i className="fab fa-whatsapp"></i>
-    </a>
-  );
+    return (
+        <a
+            href={`https://api.whatsapp.com/send?phone=${phoneNumber}`}
+            className="whatsapp-button"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Chat on WhatsApp"
+        >
+            <i className="fab fa-whatsapp"></i>
+        </a>
+    );
 }
 
 export default Whatsapp;
