a9cc3fa40b93c5b9619e16726f9e0c63bafe97d0
anish-stack <anishjha896@gmail.com>
2025-05-23 17:13:27 +0530

    feat: Add UserApp component and integrate it into Home page
    - Created UserApp component with app description, download buttons, features, and screenshots.
    - Added images for the app screenshots.
    - Introduced app-styles.css for styling the UserApp component.
    - Integrated UserApp into the Home page layout.
    - Updated order.Controller.js with minor comment adjustments.

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -10,6 +10,7 @@ import HomeBanner from '../../Components/HomeInnerBanner/HomeBanner'
 import OurValue from '../../Components/Value/OurValue'
 import MetaTag from '../../Components/Meta/MetaTag'
 import GetServicePopup from '../../Components/getservicepopup/GetServicePopup'
+import UserApp from '../AppComponent/UserApp'
 
 function Home() {
   const [isPopUp, setPopUp] = React.useState(false)
@@ -37,6 +38,7 @@ function Home() {
       <MetaTag title={'Find The Best Quality of HVAC solutions at Blueace'} description={'Welcome to Blueace provides top-notch heating, ventilation, and air conditioning solutions where comfort meets expertise. Contact us today for more details: +91 9311539090'} keyword='HVAC solutions' focusKeywords={'HVAC solutions'} />
       <Hero />
       <CategoryHome />
+      <UserApp/>
       <FeatureListHome />
       <AboutUs />
       <OurValue />
